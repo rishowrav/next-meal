b1d29a689d2a3b2540633c02a6a66f271065e02f
rishowrav <rishowrav@gmail.com>
2024-06-27 22:10:20 +0600

    fourth commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-const getTime = async () => {
-  const res = await fetch("https://next-meal-vita.vercel.app/time", {
-    next: { revalidate: 5 },
-  });
-  const data = await res.json();
-  return data.currentTime;
-};
+// const getTime = async () => {
+//   const res = await fetch("https://next-meal-vita.vercel.app/time", {
+//     next: { revalidate: 5 },
+//   });
+//   const data = await res.json();
+//   return data.currentTime;
+// };
 
 const page = async () => {
-  const currentTime = await getTime();
+  // const currentTime = await getTime();
 
   return (
     <div>
       <h6 className="text-3xl">About Page</h6>
-      <h3 className="text-3xl text-red-400">Time: {currentTime}</h3>
+      {/* <h3 className="text-3xl text-red-400">Time: {currentTime}</h3> */}
     </div>
   );
 };
